9b7b4789260d7174b2811d752e15123959473f0f
AidanTallon <atallon@spartaglobal.co>
2017-04-13 19:34:46 +0100

    Add test email method

--- app.rb
+++ app.rb
@@ -1,6 +1,15 @@
 require 'sinatra'
 require 'dotenv/load'
+require 'mail'
 
 get '/' do
   'test'
+end
+
+post '/message/:body' do
+  mail = Mail.new do
+    from ENV['EMAIL']
+    to ENV['EMAIL']
+    subject 'test'
+    body params[:body]
 end
