6a80330cfb5343c93133a9e4bb59d9875cdd4ee1
AidanTallon <atallon@spartaglobal.co>
2017-04-13 20:56:33 +0100

    add basic auth to post

--- app.rb
+++ app.rb
@@ -2,17 +2,32 @@ require 'sinatra'
 require 'dotenv/load'
 require 'mongo'
 
+require 'digest'
+
 require 'pry'
 
 client = Mongo::Client.new(['127.0.0.1:27017'], :database => 'test')
 db = client.database
+
+sha256 = Digest::SHA256.new
+
+def auth_user(password)
+  binding.pry
+  if sha256.digest(password) == ENV['HASH']
+    return true
+  else
+    return false
+  end
+end
 
 get '/' do
   'test'
 end
 
 post '/test' do
-  db[:test].insert_one(name: params['stuff']).to_s
+  if auth_user params['user']
+    db[:test].insert_one(name: params['stuff']).to_s
+  end
 end
 
 get '/test' do
