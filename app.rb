55d0e39fddd42a7b10c22cc427b6e08f22cae56a
AidanTallon <atallon@spartaglobal.co>
2017-04-13 21:38:50 +0100

    TODO: security;

--- app.rb
+++ app.rb
@@ -2,20 +2,13 @@ require 'sinatra'
 require 'dotenv/load'
 require 'mongo'
 
-require 'digest'
-
 require 'pry'
 
 client = Mongo::Client.new(['127.0.0.1:27017'], :database => 'test')
 db = client.database
 
 def auth_user(password)
-  sha256 = Digest::SHA256.new
-  if sha256.digest(password) == ENV['HASH']
-    return true
-  else
-    return false
-  end
+  password == ENV['PASSWORD']
 end
 
 get '/' do
