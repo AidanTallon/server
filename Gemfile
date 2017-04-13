069eaca6054b9435b3ec9e8450906db6bf1e27e1
AidanTallon <atallon@spartaglobal.co>
2017-04-13 19:34:46 +0100

    Add test email method

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 
 gem 'sinatra', '~>1.4.8'
 gem 'dotenv', '~>2.2.0'
+gem 'mail', '~>2.6.4'
 
 group :development do
   gem 'pry', '~>0.10.4'
--- app.rb
+++ app.rb
@@ -1,6 +1,16 @@
 require 'sinatra'
 require 'dotenv/load'
+require 'mail'
 
 get '/' do
   'test'
+end
+
+post '/message/:body' do
+  mail = Mail.deliver do
+    from ENV['EMAIL']
+    to ENV['EMAIL']
+    subject 'test'
+    body params[:body]
+  end
 end
