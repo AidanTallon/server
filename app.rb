53b0886c1ccee0d86c0aa277b4a61d1a8c4be9b2
AidanTallon <atallon@spartaglobal.co>
2017-04-14 12:50:46 +0100

    Add post transaction method

--- app.rb
+++ app.rb
@@ -7,6 +7,8 @@ require 'pry'
 client = Mongo::Client.new(['127.0.0.1:27017'], :database => 'test')
 db = client.database
 
+db_transaction = db[:transaction]
+
 def auth_user(password)
   password == ENV['PASSWORD']
 end
@@ -17,10 +19,20 @@ end
 
 post '/test' do
   if auth_user params['user']
-    db[:test].insert_one(name: params['stuff']).to_s
+    input = params
+    input.delete('user')
+    db[:test].insert_one(name: params).to_s
   end
 end
 
 get '/test' do
-  db[:test].find(name: params['stuff']).first.to_s
+  db[:test].find(params).first.to_s
+end
+
+post '/transaction' do
+  if auth_user params['user']
+    input = params
+    input.delete('user')
+    db_transaction.insert_one(params)
+  end
 end
